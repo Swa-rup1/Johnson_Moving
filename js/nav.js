412366b07096a28a1706a68b9c650107c4bf2100
Swarup Bamjan <bamjanswarup666@gmail.com>
2024-02-05 14:19:53 +0545

    first commit

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+// Dropdown JS
+function showDropdown(element) {
+  const dropdown = element.querySelector(".dropdown-menu");
+  dropdown.classList.add("show");
+}
+
+function hideDropdown(element) {
+  const dropdown = element.querySelector(".dropdown-menu");
+  dropdown.classList.remove("show");
+}
